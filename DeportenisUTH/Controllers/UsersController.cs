35bf705a48a100f4c2ad09d24c3c0cdd3a5696fb
DESKTOP-KL11P2L\jesus <jesusmrin29@gmail.com>
2023-11-20 17:05:58 -0700

    Se agrego los dashborads Catalogos y Seguridad, en seguridad Usuarios se agrego el diseño registrar usuarios

---
+++ DeportenisUTH/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using API.Models;
+using Database;
+
+namespace DeportenisUTH.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            User model = new User();
+
+            API.Catalogs.Users api = new API.Catalogs.Users();
+            model.Name = "name";
+            model.LastName = "asd";
+            model.Email = "email";
+            model.Password = "password";
+            model.Rol = "admin";
+            model.LastCreated = DateTime.Now;
+            model.LastUpdated = DateTime.Now;
+            result = api.Add(model);
+            return View();
+        }
+    }
+}
